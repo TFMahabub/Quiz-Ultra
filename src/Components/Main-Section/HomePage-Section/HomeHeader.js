5588de085c79e76727b49c008465aeb02b85e70a
TFMahabub <rjmahabub543@gmail.com>
2022-10-11 03:44:03 +0600

    homepage Header Section done

--- src/Components/Main-Section/HomePage-Section/HomeHeader.js
+++ src/Components/Main-Section/HomePage-Section/HomeHeader.js
@@ -3,9 +3,9 @@ import img from './Quizs.png'
 
 const HomeHeader = () => {
   return (
-    <div className='flex items-center justify-between mx-20 mt-4 border-2'>
+    <div className='flex items-center justify-between mx-3 md:mx-36 mt-4 py-2 px-6 border-2'>
       <img className='w-20' src={img} alt="" />
-      <h2>Thank you for visiting my site :</h2>
+      <h2 className='text-xl font-semibold text-gray-700'>Thank you for visiting my site</h2>
     </div>
   );
 };
--- src/Components/Main-Section/HomePage-Section/HomePage.js
+++ src/Components/Main-Section/HomePage-Section/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
     <div>
       <HomeHeader></HomeHeader>
-      <div className='grid md:grid-cols-2 lg:grid-cols-4 mx-3 md:mx-36 gap-8 md:gap-4 mt-12 md:mt-32'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-4 mx-3 md:mx-36 gap-8 md:gap-4 mt-12'>
        {
         quizData.map(singleData => 
         <QuizCard
